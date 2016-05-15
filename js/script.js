9c2b373096b771f7fc3f9aa452807b27f9c84206
Harpreet Riat <harpreet@ebi.ac.uk>
2016-05-15 16:45:30 +0100

    New structure

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $('._widget').each(function() {
-    $(this).closest('.panel').find('._widget_heading h2').append('&nbsp;<a class="btn btn-default btn-xs" href="#">-</a>').find('a').on('click', { widget: $(this) }, function(e) {
-      e.preventDefault();
-      e.data.widget.slideToggle();
-      this.innerHTML = this.innerHTML === '-' ? '+' : '-';
-    });
-  });
-});
